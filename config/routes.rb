00ece4a938ff879005f6ad6db1ea5cf0c7044e7b
xiejianan <xiejianan86@gmail.com>
2017-05-16 23:17:27 +0800

    add cart

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :carts
 end
